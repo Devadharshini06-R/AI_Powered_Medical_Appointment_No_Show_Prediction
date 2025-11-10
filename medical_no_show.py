67c60e2a07f0ee56cacb2fb230a527e707926d02
Devadharshini Radhakrishnan <devadharshini.radhakrishnan06@gmail.com>
2025-11-10 11:49:31 +0530

    Update medical_no_show.py

--- medical_no_show.py
+++ medical_no_show.py
@@ -36,8 +36,8 @@ st.markdown("---")
 # =============================================
 # CONFIGURE YOUR FILE PATHS HERE
 # =============================================
-MODEL_PATH = "C:\\Users\\krish\\OneDrive\\Desktop\\medical_no_show\\no_show_medical.pkl" 
-DATA_PATH = "C:\\Users\\krish\\OneDrive\Desktop\\medical_no_show\\medical_final.csv"
+MODEL_PATH = "no_show_medical.pkl" 
+DATA_PATH = "medical_final.csv"
 
 # Load model automatically
 @st.cache_resource
@@ -579,3 +579,4 @@ GradientBoostingClassifier(
         title="Model Performance Radar Chart", height=600
     )
     st.plotly_chart(fig_radar, use_container_width=True)
+
